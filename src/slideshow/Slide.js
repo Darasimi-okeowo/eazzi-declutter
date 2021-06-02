940a157fdb68f894045bd76597152ed3249daa4b
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-06-02 12:18:03 +0100

    third-commit

--- src/slideshow/Slide.js
+++ src/slideshow/Slide.js
@@ -5,28 +5,27 @@ import car from './car.png'
 import ps5 from './ps5.png'
 import chair from './chair.png'
 import iphone from './iphone.png'
-// import './style.css';
+import './slide.css'
 
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2 },
     { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
+    // { width: 1200, itemsToShow: 4 },
   ];
 
 const Slide = () => {
             return (
-              <>
-                <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
-                <div className="App">
+              <div className=''>
+                <div className="car">
                   <Carousel breakPoints={breakPoints}>
-                    <Item> <img src={car} alt=''/> </Item>
-                    <Item> <img src={ps5} alt=''/> </Item>
-                    <Item> <img src={chair} alt=''/> </Item>
-                    <Item> <img src={iphone} alt=''/> </Item>
+                    <Item> <img src={car} alt='' className='think'/> </Item>
+                    <Item> <img src={ps5} alt='' className='think'/> </Item>
+                    <Item> <img src={chair} alt='' className='think'/> </Item>
+                    <Item> <img src={iphone} alt='' className='think'/> </Item>
                   </Carousel>
                 </div>
-              </>
+              </div>
             );
 }
 
